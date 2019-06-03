7fb0e63d5471f543fb7657b3af6a8d1f0fde9e09
Thapelo Motene <thapelo.motene@gmail.com>
2019-06-03 20:57:59 +0200

    added cors

--- server.js
+++ server.js
@@ -21,7 +21,18 @@ mongoose.connect(DB_STRING,{useNewUrlParser:true}, (err)=>{
 
 
 const app = express();
-app.use(cors());
+var whitelist = ['http://localhost:3000']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+app.use(cors(corsOptions));
 
 
 
