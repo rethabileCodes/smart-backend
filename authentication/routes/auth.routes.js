35a7e051085fd21bf477c4213d758b3e4b8576b0
Thapelo Motene <thapelo.motene@gmail.com>
2019-06-04 19:09:26 +0200

    invalid user response

--- authentication/routes/auth.routes.js
+++ authentication/routes/auth.routes.js
@@ -60,7 +60,7 @@ router.post('/login', (req,res,next) =>{
         if (err) { return next(err); }
 
       console.log(user)
-        if (!user) { return res.status(404).json(info); }
+        if (!user) { return res.json(info); }
         
         req.login(user, function(err) {
           if (err) { return next(err); }
